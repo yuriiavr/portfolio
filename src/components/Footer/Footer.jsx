9feb8e7f86178c0f964c37d5ea46a13050638e58
yuriiavr <yurii.avramets@gmail.com>
2023-03-11 15:08:31 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,15 +7,15 @@ const Footer = () => {
             <div className={css.footerCont}>
                 <div className={css.copyCont}>
                     <span className={css.columnHeader}>© 2021–2023</span>
-                    <a className={css.footerlink} href="/">Yurii Avramets</a>
+                    <a className={css.footerlink} href="https://yuriiavr.github.io/portfolio/">Yurii Avramets</a>
                 </div>
                 <div className={css.socialCont}>
                     <span className={css.columnHeader}>Contacts</span>
                     <a className={css.footerlink} href="tel:+380931848293">+380931848293</a>
                 </div>
                 <div className={css.iconCont}>
-                    <a className={css.iconlink} href="/"><img src={require('../../assets/images/tg.png')} width='30' alt="" /></a>
-                    <a className={css.iconlink} href="/"><img src={require('../../assets/images/git.png')} width='30' alt="" /></a>
+                    <a className={css.iconlink} href="https://yuriiavr.github.io/portfolio/"><img src={require('../../assets/images/tg.png')} width='30' alt="" /></a>
+                    <a className={css.iconlink} href="https://yuriiavr.github.io/portfolio/"><img src={require('../../assets/images/git.png')} width='30' alt="" /></a>
                 </div>
             </div>
         </Container>
