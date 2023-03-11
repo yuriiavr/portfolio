2e4f4e3c2f5c9884ff982cc8ec2fc4d6a3257cb6
yuriiavr <yurii.avramets@gmail.com>
2023-03-11 15:12:50 +0300

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
             <div>
                 <span className={css.hello}>Hello, my name is Yura</span>
                 <h1 className={css.title}>I'm Front-End Developer.</h1>
-                <span className={css.subtitle}>Recently Im starting my way like a Front-End Developer. My tech skills stack now are JavaScript, React, React Native and Node.js. I stadied at <a className={css.textLink} href="https://www.goit.global/">GoIT</a> and made there some pet projects. Now I'm creating my own projects, you can see them below. So this website is something like my portfolio and blog. Welcome.</span>
+                <span className={css.subtitle}>Recently I started my way like a Front-End Developer. My tech skills stack now are JavaScript, React, React Native and Node.js. I stadied at <a className={css.textLink} href="https://www.goit.global/">GoIT</a> and made there some pet projects. Now I'm creating my own projects, you can see them below. So this website is something like my portfolio and blog. Welcome.</span>
             </div>
             <div className={css.projectsCont}>
                 <h2 className={css.sectionName}>My Projects</h2>
